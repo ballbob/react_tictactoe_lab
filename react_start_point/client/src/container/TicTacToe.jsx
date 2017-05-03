0a25f8fe973f4933690c52705b2f17ca2140d47a
Robert Ball <robert.ball.1989@gmail.com>
2017-05-03 16:40:23 +0100

    Winner state works.

--- react_start_point/client/src/container/TicTacToe.jsx
+++ react_start_point/client/src/container/TicTacToe.jsx
@@ -29,32 +29,39 @@ class TicTacToe extends React.Component{
     this.findWinner()
   }
 
+  containsAll(squares,sq1, sq2, sq3){
+    if(squares.includes(sq1) && squares.includes(sq2) && squares.includes(sq3)){
+      return true
+    }
+    else return false
+  }
+
   findWinner(){
     if(
-      this.state.xSquares.containsAll("1a","1b","1c") ||
-      this.state.xSquares.containsAll("2a","2b","2c") ||
-      this.state.xSquares.containsAll("3a","3b","3c") ||
-      this.state.xSquares.containsAll("1a","2a","3a") ||
-      this.state.xSquares.containsAll("1b","2b","3b") ||
-      this.state.xSquares.containsAll("1c","2c","3c") ||
-      this.state.xSquares.containsAll("1a","2b","3c") ||
-      this.state.xSquares.containsAll("1c","2b","3a")
+      this.containsAll(this.state.xSquares,"1a","1b","1c") ||
+      this.containsAll(this.state.xSquares,"2a","2b","2c") ||
+      this.containsAll(this.state.xSquares,"3a","3b","3c") ||
+      this.containsAll(this.state.xSquares,"1a","2a","3a") ||
+      this.containsAll(this.state.xSquares,"1b","2b","3b") ||
+      this.containsAll(this.state.xSquares,"1c","2c","3c") ||
+      this.containsAll(this.state.xSquares,"1a","2b","3c") ||
+      this.containsAll(this.state.xSquares,"1c","2b","3a")
       ){
       return(
-        alert("X wins")
+        console.log('X wins')
         )
     }else if(
-      this.state.oSquares.containsAll("1a","1b","1c") ||
-      this.state.oSquares.containsAll("2a","2b","2c") ||
-      this.state.oSquares.containsAll("3a","3b","3c") ||
-      this.state.oSquares.containsAll("1a","2a","3a") ||
-      this.state.oSquares.containsAll("1b","2b","3b") ||
-      this.state.oSquares.containsAll("1c","2c","3c") ||
-      this.state.oSquares.containsAll("1a","2b","3c") ||
-      this.state.oSquares.containsAll("1c","2b","3a")
+      this.containsAll(this.state.oSquares,"1a","1b","1c") ||
+      this.containsAll(this.state.oSquares,"2a","2b","2c") ||
+      this.containsAll(this.state.oSquares,"3a","3b","3c") ||
+      this.containsAll(this.state.oSquares,"1a","2a","3a") ||
+      this.containsAll(this.state.oSquares,"1b","2b","3b") ||
+      this.containsAll(this.state.oSquares,"1c","2c","3c") ||
+      this.containsAll(this.state.oSquares,"1a","2b","3c") ||
+      this.containsAll(this.state.oSquares,"1c","2b","3a")
     ){
       return (
-        alert("O Wins")
+        console.log('O wins')
         )
     }
 
