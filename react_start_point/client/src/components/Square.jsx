dcb285fc2cd8202e87d014a1bfbe89602e0e3c18
Robert Ball <robert.ball.1989@gmail.com>
2017-05-03 13:19:05 +0100

    Got Os to be added to the squares in th board onclick

--- react_start_point/client/src/components/Square.jsx
+++ react_start_point/client/src/components/Square.jsx
@@ -6,17 +6,23 @@ class Square extends React.Component{
     super(props)
 
     this.handleClick = this.handleClick.bind(this)
+
+    this.state = {
+      symbol: null
+    }
   }
 
   handleClick(){
     console.log('What is this in the square? ', this)
     console.log("What is the square's row? ", this.props.row)
     console.log("What is the square's column? ", this.props.column)
+    this.setState({symbol: "O"})
   }
 
   render(){
     return (
         <span className="square" onClick={this.handleClick}>
+          {this.state.symbol}
         </span>
     )
   }
